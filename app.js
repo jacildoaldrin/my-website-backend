6909807d555698bcc2e51d72eb9fbf8463a4c3a6
Aldrin Jacildo <ajacildo1999@gmail.com>
2020-09-04 16:04:17 -0400

    edited appjs file

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const messageRoute = require("./routes/Message");
 
 const cors = require("cors");
 app.use(cors());
@@ -9,22 +10,17 @@ app.use(cors());
 require("dotenv/config");
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
-
-var port = process.env.PORT || 5000
+app.use(bodyParser.json());
 
 // routes
-const messageRoute = require("./routes/Message");
-
 app.use("/messages", messageRoute);
 
-
-mongoose.set('useUnifiedTopology', true);
+mongoose.set("useUnifiedTopology", true);
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
   console.log("Successful connection to DB!!!");
 });
 
-app.listen(port);
+app.listen(3010, () => console.log("App listening on port 3010"));
